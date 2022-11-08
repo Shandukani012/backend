3c28e6425daff8a03079b2a49e5eba09940454de
Shandukani012 <111298700+Shandukani012@users.noreply.github.com>
2022-11-08 09:01:22 -0800

    first commit

---
+++ node-api-postgres/queries.js
@@ -0,0 +1,73 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'admin',
+  host: 'localhost',
+  database: 'api',
+  password: 'admin12345',
+  port: 5432,
+})
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createUser = (request, response) => {
+  const { title,price,image,discription } = request.body
+
+  pool.query('INSERT INTO public.users (title,price,image,discription) VALUES ($1, $2,$3,$4)', [title,price,image,discription], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results.insertId}`)
+  })
+}
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { title,price,image,discription } = request.body
+
+  pool.query(
+    'UPDATE users SET title = $1, price = $2, image= $3, discription = $4 WHERE id = $3',
+    [title,price, image,discription,id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+}
